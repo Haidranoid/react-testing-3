9c9b2a6393039be86db5c77951dd81b36faaa211
Jose Eduardo Martinez Lara <eduardo.martinez@agavelab.com>
2022-10-17 21:26:46 -0500

    Tests and code for non-zero guessed words

--- src/GuessedWords..js
+++ src/GuessedWords..js
@@ -1,9 +1,27 @@
 const GuessedWords = ({guessedWords}) => {
 
     return <div data-test="component-guessed-words">
-        {guessedWords.length === 0 && <span data-test="guess-instructions">
-            Try to guess the secret word!
-        </span>}
+        {guessedWords.length === 0
+            ? <span data-test="guess-instructions">Try to guess the secret word!</span>
+            : <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Guess</th>
+                        <th>Matching Letters</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {guessedWords.map((word, index) => (
+                        <tr data-test="guessed-word" key={index}>
+                            <td>{word.guessedWord}</td>
+                            <td>{word.letterMatchCount}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>}
     </div>
 }
 
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -8,16 +8,16 @@ const defaultProps = {
     ]
 }
 
-describe("GuessedWords", () => {
-
-    const setup = (props = {}) => {
-        const setupProps = {
-            ...defaultProps,
-            ...props,
-        }
-
-        return shallow(<GuessedWords {...setupProps}/>)
+const setup = (props = {}) => {
+    const setupProps = {
+        ...defaultProps,
+        ...props,
     }
+
+    return shallow(<GuessedWords {...setupProps}/>)
+}
+
+describe("GuessedWords - if there are no words guessed", () => {
 
     let wrapper;
     beforeEach(() => {
@@ -30,10 +30,42 @@ describe("GuessedWords", () => {
         expect(component.length).toBe(1);
     });
 
-    test("render intructions to guess a word", () => {
+    test("render instructions to guess a word", () => {
         const instructions = findByTestAttribute(wrapper, 'guess-instructions');
 
         expect(instructions.text().length).not.toBe(0);
     });
 
+})
+
+describe("GuessedWords - if there are words guessed", () => {
+
+    const guessedWords = [
+        {guessedWord: 'train', letterMatchCount: 3},
+        {guessedWord: 'agile', letterMatchCount: 1},
+        {guessedWord: 'party', letterMatchCount: 5},
+    ]
+
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({guessedWords});
+    })
+
+    test("renders without error", () => {
+        const component = findByTestAttribute(wrapper, 'component-guessed-words');
+
+        expect(component.length).toBe(1);
+    });
+
+    test("renders 'guessed words' section", () => {
+        const guessedWordsNode = findByTestAttribute(wrapper, 'guessed-words');
+
+        expect(guessedWordsNode.length).toBe(1);
+    });
+
+    test("correct number of guessed words", () => {
+        const guessWordsNodes = findByTestAttribute(wrapper, 'guessed-word');
+
+        expect(guessWordsNodes.length).toBe(3);
+    });
 })
