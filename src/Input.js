17c84e1c0a8bf79fbe72606032aaa9fdb268f46e
Jose Eduardo Martinez Lara <eduardo.martinez@agavelab.com>
2022-10-17 23:12:46 -0500

    React.useState function mocked

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,21 @@
-const Input = () => {
-    return <div data-test="input-component">
+import React from "react";
 
+const Input = ({secretWord}) => {
+    const [currentGuess, setCurrentGuess] = React.useState('');
+
+    return <div data-test="input-component">
+        <form className="form-inline">
+            <input type="text"
+                   data-test="input-box"
+                   className="mb-2 mx-sm-3"
+                   placeholder="enter guess"
+                   value={currentGuess}
+                   onChange={e => setCurrentGuess(e.target.value)}
+            />
+            <button data-test="submit-button" className="btn btn-primary mb-2">
+                Submit
+            </button>
+        </form>
     </div>
 }
 
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,10 +1,13 @@
 import {shallow} from "enzyme";
+import React from "react";
 import Input from "./Input";
 import {findByTestAttribute} from "../test/test-utils";
 
+
+const setup = () => shallow(<Input/>);
+
 describe("Input", () => {
 
-    const setup = () => shallow(<Input/>);
     test("renders without error", () => {
         const wrapper = setup();
 
@@ -12,3 +15,19 @@ describe("Input", () => {
         expect(inputComponent.length).toBe(1);
     })
 });
+
+describe("Input - state controlled input field", () => {
+
+    test("state updates will value of input box upon change", () => {
+        const mockSetCurrentGuess = jest.fn();
+        React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+        const wrapper = setup();
+        const inputBox = findByTestAttribute(wrapper, 'input-box');
+
+        const mockEvent = {target: {value: 'train'}};
+        inputBox.simulate("change", mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    })
+})
