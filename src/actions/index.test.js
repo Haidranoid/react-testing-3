524c617817be0c33c1ec24966ea25c665667a47b
Jose Eduardo Martinez Lara <eduardo.martinez@agavelab.com>
2022-10-24 21:43:04 -0500

    Add CORRECT_GUESS action creator and test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,5 +1,12 @@
 const moxios = require('moxios')
-import {getSecretWord} from "./index";
+import {getSecretWord, correctGuess, actionTypes} from "./index";
+
+describe("correctGuess", () => {
+    test("returns an action with type 'CORRECT_GUESS", () => {
+        const action = correctGuess();
+        expect(action).toStrictEqual({type: actionTypes.CORRECT_GUESS})
+    })
+})
 
 describe("getSecretWord", () => {
     beforeEach(() => {
@@ -10,7 +17,7 @@ describe("getSecretWord", () => {
         moxios.uninstall();
     })
 
-    test("secretWord is returned",  () => {
+    test("secretWord is returned", () => {
         moxios.wait(() => {
             const request = moxios.requests.mostRecent();
 
